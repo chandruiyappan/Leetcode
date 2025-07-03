6f157abfcd52228d98cf2092fd6565880924b3c5
chandruiyappan <123877158+chandruiyappan@users.noreply.github.com>
2025-07-03 09:15:45 +0530

    Time: 2794 ms (65.71%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,3 +1,4 @@
-/* Write your T-SQL query statement below */
-SELECT Product.product_name, Sales.year,Sales.price FROM Sales
- LEFT JOIN Product ON Product.product_id = Sales.product_id;
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+INNER JOIN Product p ON s.Product_id = p.Product_id
+ORDER BY  p.product_name
