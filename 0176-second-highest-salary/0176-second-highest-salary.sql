59d25f197f0ff7c93967b3037852a82205de80ce
chandruiyappan <123877158+chandruiyappan@users.noreply.github.com>
2025-07-05 11:23:37 +0530

    Time: 391 ms (47.68%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,13 @@
-SELECT(
-    SELECT DISTINCT salary FROM Employee
-    ORDER BY salary DESC
-    OFFSET 1 ROWS FETCH NEXT 1 ROWS ONLY
-)AS SecondHighestSalary;
+WITH RankedSalaries AS (
+    SELECT 
+        DISTINCT salary,
+        DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+    FROM Employee
+)
+
+SELECT 
+    CASE 
+        WHEN EXISTS (SELECT 1 FROM RankedSalaries WHERE rnk = 2)
+        THEN (SELECT salary FROM RankedSalaries WHERE rnk = 2)
+        ELSE NULL
+    END AS SecondHighestSalary;
