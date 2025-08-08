a2f4fe8a53988110b11f1b756bd88e84c9bb009d
chandruiyappan <123877158+chandruiyappan@users.noreply.github.com>
2025-08-08 10:35:53 +0530

    Time: 236 ms (79.23%), Space: 20.3 MB (57.76%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,16 +1,17 @@
 class Solution:
     @staticmethod
-    def numIslands(g):
-        def dfs(i, j):
-            if 0 <= i < len(g) and 0 <= j < len(g[0]) and g[i][j] == "1":
-                g[i][j] = "0"
-                for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                    dfs(i + x, j + y)
+    def dfs(g, i, j):
+        if 0 <= i < len(g) and 0 <= j < len(g[0]) and g[i][j] == "1":
+            g[i][j] = "0"
+            for x, y in [(1,0), (-1,0), (0,1), (0,-1)]:
+                Solution.dfs(g, i+x, j+y)
 
+    @staticmethod
+    def numIslands(g):
         c = 0
         for i in range(len(g)):
             for j in range(len(g[0])):
                 if g[i][j] == "1":
-                    dfs(i, j)
+                    getattr(Solution, 'dfs')(g, i, j)
                     c += 1
         return c
